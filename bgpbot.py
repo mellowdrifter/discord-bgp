497ebf836b4fb45b64c1a17b3ba473e10d2ce4dc
Darren O'Connor <mellow.drifter@gmail.com>
2022-06-24 00:59:33 +0000

    add geoip

--- bgpbot.py
+++ bgpbot.py
@@ -12,22 +12,36 @@ from typing import Dict, List
 
 load_dotenv()
 
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv("DISCORD_TOKEN")
 ALERTKEY = "%"
 ONE_MINUTE = 60
 FIVE_MINUTES = 5 * ONE_MINUTE
 ONE_HOUR = 60 * ONE_MINUTE
 TWENTY_FOUR_HOURS = 24 * ONE_HOUR
 
-COMMANDS = ["route", "origin", "aspath", "roa", "help",
-            "asname", "invalids", "totals", "sourced", "vrps"]
+COMMANDS = [
+    "route",
+    "origin",
+    "aspath",
+    "roa",
+    "help",
+    "asname",
+    "invalids",
+    "totals",
+    "sourced",
+    "vrps",
+    "geoip",
+]
 
 logging.basicConfig(
-    level=logging.INFO, format='%(asctime)s: %(levelname)s: %(message)s')
+    level=logging.INFO, format="%(asctime)s: %(levelname)s: %(message)s"
+)
 
 
 async def send_help(channel):
-    return await channel.send(quote("""
+    return await channel.send(
+        quote(
+            """
 bgpstuff.net help.
 
 Commands:
@@ -36,11 +50,14 @@ Commands:
   % aspath - Returns the AS path I see to get to the passed in IP address
   % roa - Returns the ROA status of the passed in IP address
   % asname - Returns the AS name from the passed in AS number
+  % geoip - Returns a guesstimate of where the IP is geo located
   % invalids - Returns all RPKI invalid prefixes advertised from the passed in AS number
   % vrps - Returns all Validated ROA Payloads for the passed in AS number
   % sourced - Returns all prefixes originated from the passed in AS number
   % totals - Returns the current IPv4 and IPv6 active prefix count
-"""))
+"""
+        )
+    )
 
 
 def no_200(status_code: int) -> str:
@@ -71,8 +88,7 @@ def quote(txt: str) -> str:
 
 def split_text_green_quote(txt: str) -> List[str]:
     splittxt = txt.split("\n")
-    rejoined = ['\n'.join(splittxt[i:i+80])
-                for i in range(0, len(splittxt), 80)]
+    rejoined = ["\n".join(splittxt[i : i + 80]) for i in range(0, len(splittxt), 80)]
     newtxt = []
     for i in range(len(rejoined)):
         if i == 0:
@@ -84,8 +100,7 @@ def split_text_green_quote(txt: str) -> List[str]:
 
 def split_text_yellow_quote(txt: str) -> List[str]:
     splittxt = txt.split("\n")
-    rejoined = ['\n'.join(splittxt[i:i+80])
-                for i in range(0, len(splittxt), 80)]
+    rejoined = ["\n".join(splittxt[i : i + 80]) for i in range(0, len(splittxt), 80)]
     newtxt = []
     for i in range(len(rejoined)):
         if i == 0:
@@ -103,7 +118,9 @@ def totals(bgp) -> str:
         return red_quote(e)
     if bgp.status_code != 200:
         return no_200(bgp.status_code)
-    return green_quote(f"I see {bgp.total_v4} IPv4 and {bgp.total_v6} IPv6 prefixes active")
+    return green_quote(
+        f"I see {bgp.total_v4} IPv4 and {bgp.total_v6} IPv6 prefixes active"
+    )
 
 
 @cached(cache=TTLCache(maxsize=20, ttl=ONE_MINUTE))
@@ -138,6 +155,25 @@ def origin(prefix: str, bgp) -> str:
     if not bgp.exists:
         return yellow_quote(f"No prefix exists for {prefix}")
     return green_quote(f"The origin AS for {prefix} is AS{bgp.origin}")
+
+
+@cached(cache=TTLCache(maxsize=50, ttl=ONE_HOUR))
+def geoip(prefix: str, bgp) -> str:
+    logging.info(f"geoip request for {prefix}")
+    try:
+        bgp.get_geoip(prefix)
+    except ValueError as ve:
+        return red_quote(ve)
+    except Exception as e:
+        logging.debug(e)
+        return
+    if bgp.status_code != 200:
+        return no_200(bgp.status_code)
+    if not bgp.exists:
+        return yellow_quote(f"No prefix exists for {prefix}")
+    city = bgp.geoip["City"]
+    country = bgp.geoip["Country"]
+    return green_quote(f"{prefix} might be located in {city}, {country}")
 
 
 @cached(cache=TTLCache(maxsize=20, ttl=ONE_MINUTE))
@@ -223,7 +259,9 @@ def invalids(asnum: int, bgp) -> str:
     invalids = all_invalids(bgp)
     if num in invalids:
         prefixes = "\n\t".join(map(str, invalids[num]))
-        return split_text_yellow_quote(f"AS{asnum} is originating the following invalid prefixes:\n\t{prefixes}")
+        return split_text_yellow_quote(
+            f"AS{asnum} is originating the following invalid prefixes:\n\t{prefixes}"
+        )
     else:
         return green_quote(f"AS{num} is not originating any invalid prefixes")
 
@@ -284,15 +322,17 @@ def sourced(asnum: int, bgp) -> str:
     if not bgp.exists:
         return yellow_quote(f"AS{asnum} is not sourcing any prefixes")
     prefixes = "\n\t".join(map(str, bgp.sourced))
-    return split_text_green_quote(f"AS{asnum} is sourcing the following prefixes:\n\t{prefixes}")
+    return split_text_green_quote(
+        f"AS{asnum} is sourcing the following prefixes:\n\t{prefixes}"
+    )
 
 
 def start(dis, bgp):
-    @ dis.event
+    @dis.event
     async def on_ready():
-        print(f'{dis.user} has connected to Discord!')
+        print(f"{dis.user} has connected to Discord!")
 
-    @ dis.event
+    @dis.event
     async def on_message(message):
         # bot should not respond to its own message
         if message.author == dis.user:
@@ -318,6 +358,14 @@ def start(dis, bgp):
 
         if request[0].lower() == "route":
             req = route(request[1], bgp)
+            if req == "":
+                return
+            logging.info(req)
+            await message.channel.send(req)
+            return
+
+        elif request[0].lower() == "geoip":
+            req = geoip(request[1], bgp)
             if req == "":
                 return
             logging.info(req)
--- test_bgpbot.py
+++ test_bgpbot.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import bgpbot
 import bgpstuff
 import unittest
-from bgpbot import *
+from unittest.mock import Mock, patch
 
 small_txt = "this is some text"
 
 
 class ClientTest(unittest.TestCase):
-    def setUp(self):
-        self.client = bgpstuff.Client()
 
     def test_quote(self):
-        self.assertEqual((f"```{small_txt}```"), quote(small_txt))
+        self.assertEqual((f"```{small_txt}```"), bgpbot.quote(small_txt))
 
     def test_red_quote(self):
-        self.assertEqual((f"```🔴 {small_txt}```"), red_quote(small_txt))
+        self.assertEqual((f"```🔴 {small_txt}```"), bgpbot.red_quote(small_txt))
+
+    @patch.object(bgpstuff.Client, 'get_totals')
+    def test_totals(self):
+        mock_bgp = Mock()
+        totals.ret
+        self.assertEqual("something", bgpbot.totals(mock_bgp))
 
 
 if __name__ == '__main__':
